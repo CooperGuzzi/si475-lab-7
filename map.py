43bfc2e6543509946b62db3d3f1b8fa617f0494f
m214368 <73185501+m214368@users.noreply.github.com>
2021-04-21 18:50:13 -0400

    some work

--- map.py
+++ map.py
@@ -2,7 +2,9 @@
 
 from math import pow, sqrt
 import argparse
-
+import pygraphviz as pgv
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # distances are implemented in here
 
@@ -17,16 +19,31 @@ class Point:
         return sqrt(pow(self.x-point.x,2)+pow(self.y-point.y,2))
 
 class Map:
-    def __init__(self, file):
+    def __init__(self):
         # read from file 
-        parser = argparse.ArgumentParser(description='Process filename')
-        parser.add_argument('filename', metavar='F', type=string, nargs='1', help='filename')
-        parser.add_argument('end', metavar='E', type=list, nargs='1', help='end')
-        parser.add_argument('start', metavar='S', type=list, nargs='1', help='start')
-        parser.parserargs() # maybe
-        for line in filename:
+        parser = argparse.ArgumentParser(description='Read in Dot file and 1-2 x y points')
+        parser.add_argument('f', help='filename')
+        parser.add_argument('e', type=int, nargs=2, help='end')
+        parser.add_argument('-s','--start', type=int, nargs=2, help='start')
+        args = parser.parse_args() # maybe
+        #open f as file
+        #for line in f: 
             # read in points
 
             # read in connections
+        #https://stackoverflow.com/questions/17947027/open-dot-formatted-graph-from-python
+        Gtmp = pgv.AGraph(f)
+        G = nx.Graph(Gtmp)
+        nx.draw(G)
+        plt.show()
+
+m = Map()
 
 
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("echo")
+args = parser.parse_args()
+print(args.echo)
+'''
