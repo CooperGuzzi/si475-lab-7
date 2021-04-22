cebfba9db3d858f185e82121d910b237c8475ffd
m214368 <73185501+m214368@users.noreply.github.com>
2021-04-21 23:44:23 -0400

    possibly works?

--- map.py
+++ map.py
@@ -4,19 +4,21 @@ from math import pow, sqrt
 import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
+#import drive
 #from networkx import Graph
 
 
 def subtract(s1,s2):
+    s1 = xy(s1)
+    s2 = xy(s2)
+    return sqrt(pow(s1[0]-s2[0],2)+pow(s1[1]-s2[1],2))
+
+def xy(s1):
     s1 = s1[2:-2]
     s1 = s1.split(',')
-    s1[0] = float(s1[0])
-    s1[1] = float(s1[1])
-    s2 = s2[2:-2]
-    s2 = s2.split(',')
-    s2[0] = float(s2[0])
-    s2[1] = float(s2[1])
-    return sqrt(pow(s1[0]-s2[0],2)+pow(s1[1]-s2[1],2))
+    x = float(s1[0])
+    y = float(s1[1])
+    return x,y
 
 
 class Map:
@@ -24,20 +26,24 @@ class Map:
         # read from file 
         parser = argparse.ArgumentParser(description='Read in Dot file and 1-2 x y points')
         parser.add_argument('f', help='filename')
-        parser.add_argument('e', type=int, nargs=2, help='end')
-        parser.add_argument('-s','--start', type=int, nargs=2, help='start')
+        parser.add_argument('e', type=float, nargs=2, help='end')
+        parser.add_argument('-s','--start', type=float, nargs=2, help='start')
         args = parser.parse_args() # maybe
         filename = args.f
         G = nx.drawing.nx_pydot.read_dot(filename)
         nx.set_edge_attributes(G, values = 1, name = 'weight')
         g = nx.Graph()
-        #start = 
+        self.virtual = True
+        if (args.start is None):
+            self.driver = Driver()
+            start = self.driver.start()
+            self.virtual = False
         beststart = 100
         beststartnode = 0
-        s1 = " "+str(args.start)+" "
+        s1 = "\""+str(args.start)+"\""
         bestend = 100
         bestendnode = 0
-        e1 = " "+str(args.e)+" "
+        e1 = "\""+str(args.e)+"\""
         for n in list(G.nodes(data=True)):
             g.add_node(n[0],label = n[1]['label'])
             if (subtract(n[1]['label'],s1) < beststart):
@@ -65,11 +71,24 @@ class Map:
         length, path = nx.single_source_dijkstra(self.graph,"start",target="end",weight='weight')
         print(length)
         print(path)
+        for i in path:
+            print(self.graph.nodes[i]['label'])
+
+    def drive(self,driver):
+        length, path = nx.single_source_dijkstra(self.graph,"start",target="end",weight='weight')
+        print(length)
+        print(path)
+        for i in path:
+            print(self.graph.nodes[i]['label'])
+            self.driver.goto(xy(self.graph.nodes[i]['label']))
 
 
 
 
 m = Map()
-m.path()
+if m.virtual:
+    m.path()
+else:
+    m.drive()
 
 
